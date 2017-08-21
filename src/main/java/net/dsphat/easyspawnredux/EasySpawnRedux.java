0d9afd80b5b71c85951d360b90a11f0d61246bd3
James Golding <me@jamesg.co.nz>
2017-08-22 00:11:34 +1200

    Add license.

--- src/main/java/net/dsphat/easyspawnredux/EasySpawnRedux.java
+++ src/main/java/net/dsphat/easyspawnredux/EasySpawnRedux.java
@@ -1,21 +1,31 @@
 package net.dsphat.easyspawnredux;
 
 import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import org.bukkit.Location;
 import java.util.HashMap;
 import java.util.Map;
 
+
+/**
+ * Copyright &copy; 2017 James Golding
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file or http://www.wtfpl.net/
+ * for more details.
+ */
 public class EasySpawnRedux extends JavaPlugin {
     private HashMap<World, Location> spawnLocations = new HashMap<World, Location>();
 
     @Override
     public void onEnable() {
-
+        loadConfig();
     }
 
-    public void reloadConfig() {
+    public void loadConfig() {
         for (World world : getServer().getWorlds()) {
             String name = world.getName();
             String format = "spawn." + name;
@@ -45,5 +55,13 @@ public class EasySpawnRedux extends JavaPlugin {
             getConfig().set(format + ".yaw", yaw);
             getConfig().set(format + ".pitch", pitch);
         }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("spawn")) {
+            if (!(sender.hasPermission(cmd.getPermission()))) return false;
+        }
+        return true;
     }
 }
